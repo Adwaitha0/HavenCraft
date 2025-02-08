1dd09b9fc4eb2e14b52224b56f85cd1d51933910
adwaitha <adwaithapk2018@gmail.com>
2025-02-08 09:14:41 +0530

    completed

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,15 +1,32 @@
-const mongoose=require('mongoose')
+require('dotenv').config()
+
+// const mongoose=require('mongoose')
 
 
-const connectDB=async()=>{
-    try{
-        const conn=await mongoose.connect('mongodb://localhost:27017/project',{});
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    }catch(error){
-        console.log(error);
+// const connectDB=async()=>{
+//     try{
+//         const conn=await mongoose.connect('mongodb://localhost:27017/project',{});
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     }catch(error){
+//         console.log(error);
+//         process.exit(1);
+//     }
+// };
+
+// module.exports=connectDB
+
+
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connected to Atlas!");
+    } catch (err) {
+        console.error("MongoDB Connection Error:", err);
         process.exit(1);
     }
 };
 
-module.exports=connectDB
-
+module.exports = connectDB;
